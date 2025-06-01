e8ea2a1f7bdef11172dec84d2ab91d444e4407aa
Emmanuel Oluwatayese <adetutuemmanueloluwatayese@gmail.com>
2025-06-01 11:41:32 +0100

    feat: working on the Skills section

--- src/components/SkillsCard.tsx
+++ src/components/SkillsCard.tsx
@@ -21,74 +21,85 @@ import php from "../assets/icons/php.svg";
 import { Card } from "@heroui/card";
 import { Tooltip } from "@heroui/tooltip";
 import { ScrollShadow } from "@heroui/scroll-shadow";
-
+import { motion } from "framer-motion";
 const SkillsCard = () => {
   const stacks = [
     {
-      href: "https://www.javascript.com/",
+     delay: 0.10, href: "https://www.javascript.com/",
       name: "JavaScript",
       icon: javascript,
     },
     {
-      href: "https://www.typescriptlang.org/",
+     delay: 0.15, href: "https://www.typescriptlang.org/",
       name: "TypeScript",
       icon: typescript,
     },
-    { href: "https://react.dev/learn", name: "React.js", icon: reactjs },
-    { href: "https://nextjs.org/docs", name: "Next.js", icon: nextjs },
-    { href: "https://nodejs.org/en", name: "Node.js", icon: nodejs },
-    { href: "https://docs.nestjs.com/", name: "Nest.js", icon: nestjs },
+    {delay: 0.20, href: "https://react.dev/learn", name: "React.js", icon: reactjs },
+    {delay: 0.12, href: "https://nextjs.org/docs", name: "Next.js", icon: nextjs },
+    {delay: 0.10, href: "https://nodejs.org/en", name: "Node.js", icon: nodejs },
+    {delay: 0.25, href: "https://docs.nestjs.com/", name: "Nest.js", icon: nestjs },
     {
-      href: "https://developer.mozilla.org/en-US/docs/Web/HTML/Guides/Viewport_meta_element",
+     delay: 0.18, href: "https://developer.mozilla.org/en-US/docs/Web/HTML/Guides/Viewport_meta_element",
       name: "HTML5",
       icon: html5,
     },
     {
-      href: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+     delay: 0.30, href: "https://developer.mozilla.org/en-US/docs/Web/CSS",
       name: "CSS3",
       icon: css,
     },
-    { href: "https://vuejs.org/", name: "Vue.js", icon: vuedotjs },
-    { href: "https://angular.dev/", name: "Angular.js", icon: angular },
-    { href: "https://www.mongodb.com/", name: "MongoDB", icon: mongodb },
-    { href: "https://expressjs.com/", name: "Express", icon: express },
-    { href: "https://www.mysql.com/", name: "MYSQL", icon: mysql },
-    { href: "https://tailwindcss.com/", name: "Tailwind", icon: tailwindcss },
-    { href: "https://laravel.com/", name: "Laravel", icon: laravel },
-    { href: "https://www.php.net/", name: "PHP", icon: php },
+    {delay: 0.10, href: "https://vuejs.org/", name: "Vue.js", icon: vuedotjs },
+    {delay: 0.20, href: "https://angular.dev/", name: "Angular.js", icon: angular },
+    {delay: 0.35, href: "https://www.mongodb.com/", name: "MongoDB", icon: mongodb },
+    {delay: 0.15, href: "https://expressjs.com/", name: "Express", icon: express },
+    {delay: 0.19, href: "https://www.mysql.com/", name: "MYSQL", icon: mysql },
+    {delay: 0.25, href: "https://tailwindcss.com/", name: "Tailwind", icon: tailwindcss },
+    {delay: 0.32, href: "https://laravel.com/", name: "Laravel", icon: laravel },
+    {delay: 0.10, href: "https://www.php.net/", name: "PHP", icon: php },
     {
-      href: "https://www.postgresql.org/",
+     delay: 0.15, href: "https://www.postgresql.org/",
       name: "PostgreSQL",
       icon: postgresql,
     },
-    { href: "https://github.com/", name: "Github.js", icon: github },
-    { href: "https://firebase.google.com/", name: "Firebase", icon: firebase },
-    { href: "https://getbootstrap.com/", name: "Bootstrap", icon: bootstrap },
+    {delay: 0.18, href: "https://github.com/", name: "Github.js", icon: github },
+    {delay: 0.28, href: "https://firebase.google.com/", name: "Firebase", icon: firebase },
+    {delay: 0.31, href: "https://getbootstrap.com/", name: "Bootstrap", icon: bootstrap },
   ];
   return (
-     <ScrollShadow className="w-full h-[500px] pb-16 lg:pb-0">
-    <div className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-5 p-5">
-      {stacks.map((stack, index) => (
+    <ScrollShadow className="w-full h-[500px] pb-16 lg:pb-0">
+      <div className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-5 p-5">
+        {stacks.map((stack, index) => (
           <Tooltip
-          key={index}
-          className="dark:bg-white bg-black text-white p-3 dark:text-black"
-          content={` ${stack?.name}`}
-          showArrow={true}
+            key={index}
+            className="dark:bg-white bg-black text-white p-3 dark:text-black"
+            content={` ${stack?.name}`}
+            showArrow={true}
           >
-          <Card className="p-3 dark:border shadow hover:shadow-lg hover:scale-110">
-            <a href={stack?.href}  className="">
-              <img
-                src={stack?.icon}
-                alt=""
-                className="text rounded-tr-lg rounded-bl-lg text-white dark:bg-white"
-                />
-              {/* <p className=" ">{stack?.name}</p> */}
-            </a>
-          </Card>
-        </Tooltip>
-      ))}
-    </div>
-      </ScrollShadow>
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{
+                delay: stack?.delay || 0.1,
+                type: "spring",
+                stiffness: 100,
+              }}
+              className="text"
+            >
+              <Card className="p-3 dark:border shadow hover:shadow-lg hover:scale-110">
+                <a href={stack?.href} className="">
+                  <img
+                    src={stack?.icon}
+                    alt=""
+                    className="text rounded-tr-lg rounded-bl-lg text-white dark:bg-white"
+                  />
+                  {/* <p className=" ">{stack?.name}</p> */}
+                </a>
+              </Card>
+            </motion.div>
+          </Tooltip>
+        ))}
+      </div>
+    </ScrollShadow>
   );
 };
 
